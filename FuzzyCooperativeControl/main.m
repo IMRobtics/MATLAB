41d4ad45700428e979143b3d257a5a6c110082b3
Zaid Pirwani <email@zaidpirwani.com>
2016-10-31 13:18:52 +0500

    matlab controlling multiple bots in v-rep
    Need to generate trajectory,
    
    Need to setup PROPER separate PIDs for all
    
    Need to replace PID with FUZZY
    
    need to add cooperation

--- FuzzyCooperativeControl/main.m
+++ FuzzyCooperativeControl/main.m
@@ -11,19 +11,19 @@ y_g = transpose(Y);
 x_g=x_g(5:length(x_g)-5);
 y_g=y_g(5:length(y_g)-5);
 
-noOfRobots       = 3;
-trajectoryOffset = 1;
+numberOfRobots   = 2;
+trajectoryOffset = 1.35;
 trajectory       = zeros(17,2);
-for i=1:noOfRobots
+for i=1:numberOfRobots
     trajectory   = [trajectory x_g+((i-1)*trajectoryOffset) y_g+((i-1)*trajectoryOffset)];
 end
-trajectory=trajectory(:,3:(noOfRobots*2+2));
+trajectory=trajectory(:,3:(numberOfRobots*2+2));
 
 figure;
 hold on; grid on;
-axis([-1 10 -1 10]);
+axis([-2 4 -1 4]);
 axis square; axis equal;
-for i=1:2:noOfRobots*2
+for i=1:2:numberOfRobots*2
    colorVal = colorVal+1;
    if(colorVal>7)
       colorVal=1;
@@ -31,17 +31,15 @@ for i=1:2:noOfRobots*2
    plot(trajectory(:,i),trajectory(:,i+1),color(colorVal),'LineWidth',2);
 end
 
-point = 5;
 Kp    = 5;
 Ki    = 0.01;
 Kd    = 0.01;
-E_k   = 0;
-e_k_1 = 0;
+E_k   = zeros(1,numberOfRobots);
+e_k_1 = zeros(1,numberOfRobots);
 steps = 0;
 
 distThresh  = 0.05;
 angleThresh = 0.2;
-
 
 % Robot and Simulation Constants
 R = 41/2;  % in mm
@@ -50,15 +48,12 @@ dt=0.05;   % 50ms
 maxVel=2*pi;
 v = maxVel;
 
-xyz    =zeros(noOfRobots,3);
-eAngle =zeros(noOfRobots,3);
-pose   =zeros(noOfRobots,3);
+xyz    =zeros(numberOfRobots,3);
+eAngle =zeros(numberOfRobots,3);
+pose   =zeros(numberOfRobots,3);
 
-LOOP   = 1;
-a = 1;
-i = 1;   % for 1 robot
-vLeft  = 0;
-vRight = 0;
+vLeft  = zeros(1,numberOfRobots);
+vRight = zeros(1,numberOfRobots);
 
 %=================================================
 vrep=remApi('remoteApi');
@@ -70,105 +65,115 @@ else
     disp('Failed connecting to remote API server');
 end
 %=================================================
-[res Ints Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'setk3Velocity_function',[i vLeft vRight],[],'',[],vrep.simx_opmode_blocking);
 [~,r1_handle]=vrep.simxGetObjectHandle(clientID,'K3_robot#', vrep.simx_opmode_blocking);
-[~,xyz(a,:)]=vrep.simxGetObjectPosition(clientID, r1_handle, -1, vrep.simx_opmode_streaming);
-[~,eAngle(a,:)]=vrep.simxGetObjectOrientation(clientID,r1_handle, -1, vrep.simx_opmode_streaming);
-pose(a,:) = [xyz(a,1) xyz(a,2) atan2(sin(eAngle(a,3)), cos(eAngle(a,3)))];
+[~,r2_handle]=vrep.simxGetObjectHandle(clientID,'K3_robot#0', vrep.simx_opmode_blocking);
+[~,r3_handle]=vrep.simxGetObjectHandle(clientID,'K3_robot#1', vrep.simx_opmode_blocking);
+r_handle = [r1_handle r2_handle r3_handle];
 
-position = animatedline;
-axis([-2.5 2.5 -2.5 2.5])
-grid on
-
-
-% while abs(pose(1,1)-goal(1,1))>0.2 && abs(pose(1,2)-goal(1,2))>0.2
-% while LOOP<10
-flag = true;
-turnFlag = true;
-StraightFlag = false;
-while flag
-%   [res ir(a,:) Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'getk3FrontIR_function',[1],[],'',[],vrep.simx_opmode_blocking);
-%   [res Ints us(a,:) Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'getk3FrontSonar_function',[1],[],'',[],vrep.simx_opmode_blocking);
-    [~,xyz(a,:)]=vrep.simxGetObjectPosition(clientID, r1_handle, -1, vrep.simx_opmode_buffer);
-    [~,eAngle(a,:)]=vrep.simxGetObjectOrientation(clientID,r1_handle, -1, vrep.simx_opmode_buffer);
-    pose(a,:) = [xyz(a,1) xyz(a,2) eAngle(a,3)];
-    x = pose(a,1);
-    y = pose(a,2);
-    theta=pose(a,3) + (pi/2);
-    theta = atan2(sin(theta), cos(theta));
-
-    dist = abs(sqrt( (y_g(point)-y)^2 + (x_g(point)-x)^2));
-
-    % 1. Calculate the heading (angle) to the goal.
-    % distance between goal and robot in x-direction
-    u_x = x_g(point)-x;     
-    % distance between goal and robot in y-direction
-    u_y = y_g(point)-y;
-    % angle from robot to goal. Hint: use ATAN2, u_x, u_y here.
-    theta_g = atan2(u_y,u_x);
-
-    % 2. Calculate the heading error.
-    % error between the goal angle and robot's angle
-    % Hint: Use ATAN2 to make sure this stays in [-pi,pi].
-    e_k = theta_g-theta;
-    e_k = atan2(sin(e_k),cos(e_k));
-
-    % 3. Calculate PID for the steering angle 
-    % error for the proportional term
-    e_P = e_k;
-
-    % error for the integral term. Hint: Approximate the integral using
-    % the accumulated error, E_k, and the error for
-    % this time step, e_k.
-    e_I = E_k + e_k*dt;
-
-    % error for the derivative term. Hint: Approximate the derivative
-    % using the previous error, e_k_1, and the
-    % error for this time step, e_k.
-    e_D = (e_k-e_k_1)/dt;
-    w = Kp*e_P + Ki*e_I + Kd*e_D;
-
-    % 4. Save errors for next time step
-    E_k = e_I;
-    e_k_1 = e_k;
-        
-    if (abs(e_k)>angleThresh) && (dist>distThresh)
-        turnFlag = true;
-        StraightFlag = false;
-        vRight = (2*v+w*L)/(2*R);
-        vLeft  = (2*v-w*L)/(2*R);    
-    elseif abs(e_k)<angleThresh
-        turnFlag = false;
-        StraightFlag = true;
-    end
-    
-    if StraightFlag
-        if dist>distThresh
-            vRight = v;
-            vLeft = v;
-        else
-            point = point+1;
-            formatSpec = 'POINT=%1d REACHED.\n';
-            fprintf(formatSpec,point-1)
-            if point>length(x_g)
-                vRight = 0;
-                vLeft = 0;
-                flag = false;
-            end
-        end
-    end
-    
-    formatSpec = 'X=%4.2f, Y=%4.2f, Th=%4.2f, Thg=%4.2f, ThERR=%4.2f, DIST=%4.2f, vL=%4.2f, vR=%4.2f\n';
-    fprintf(formatSpec,x,y,theta,theta_g,e_k,dist,vLeft,vRight)
-    
-    [res Ints Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'setk3Velocity_function',[i vLeft vRight],[],'',[],vrep.simx_opmode_blocking);
-    addpoints(position,x,y);
-    drawnow
-    LOOP = LOOP+1;
-    steps = steps+1;
+for i=1:numberOfRobots
+    [~,xyz(i,:)]=vrep.simxGetObjectPosition(clientID, r_handle(i), -1, vrep.simx_opmode_streaming);
+    [~,eAngle(i,:)]=vrep.simxGetObjectOrientation(clientID,r_handle(i), -1, vrep.simx_opmode_streaming);
+    pose(i,:) = [xyz(i,1) xyz(i,2) atan2(sin(eAngle(i,3)), cos(eAngle(i,3)))];
 end
 
-vLeft = 0;
-vRight = 0;
-[res Ints Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'setk3Velocity_function',[i vLeft vRight],[],'',[],vrep.simx_opmode_blocking);
+robotTrajectory = [animatedline('Color',color(colorVal),'LineWidth',2)];
+for i = 1: numberOfRobots-1
+   colorVal = colorVal+1;
+   if(colorVal>7)
+       colorVal=1;
+   end
+   robotTrajectory = [robotTrajectory;animatedline('Color',color(colorVal),'LineWidth',2)];
+end
+
+flag = ones(1,numberOfRobots);
+turnFlag = ones(1,numberOfRobots);
+StraightFlag = zeros(1,numberOfRobots);
+moving = ones(1,numberOfRobots);
+LOOP=0;
+goalVals = [1 3 5; 2 4 6];
+while LOOP < length(trajectory)
+    LOOP = LOOP+1;
+    while sum(moving)>0
+        for i=1:numberOfRobots
+            x_g = trajectory(LOOP,goalVals(1,i));
+            y_g = trajectory(LOOP,goalVals(2,i));
+            [~,xyz(i,:)]=vrep.simxGetObjectPosition(clientID, r_handle(i), -1, vrep.simx_opmode_buffer);
+            [~,eAngle(i,:)]=vrep.simxGetObjectOrientation(clientID,r_handle(i), -1, vrep.simx_opmode_buffer);
+            pose(i,:) = [xyz(i,1) xyz(i,2) atan2(sin(eAngle(i,3)), cos(eAngle(i,3)))];
+
+            x = pose(i,1);
+            y = pose(i,2);
+            theta=pose(i,3) + (pi/2);
+            theta = atan2(sin(theta), cos(theta));
+
+            dist = abs(sqrt( (y_g-y)^2 + (x_g-x)^2));
+
+            % 1. Calculate the heading (angle) to the goal.
+            % distance between goal and robot in x-direction
+            u_x = x_g-x;     
+            % distance between goal and robot in y-direction
+            u_y = y_g-y;
+            % angle from robot to goal. Hint: use ATAN2, u_x, u_y here.
+            theta_g = atan2(u_y,u_x);
+
+            % 2. Calculate the heading error.
+            % error between the goal angle and robot's angle
+            % Hint: Use ATAN2 to make sure this stays in [-pi,pi].
+            e_k = theta_g-theta;
+            e_k = atan2(sin(e_k),cos(e_k));
+
+            % 3. Calculate PID for the steering angle 
+            % error for the proportional term
+            e_P = e_k;
+
+            % error for the integral term. Hint: Approximate the integral using
+            % the accumulated error, E_k, and the error for
+            % this time step, e_k.
+            e_I = E_k(1,i) + e_k*dt;
+
+            % error for the derivative term. Hint: Approximate the derivative
+            % using the previous error, e_k_1, and the
+            % error for this time step, e_k.
+            e_D = (e_k-e_k_1(1,i))/dt;
+            w = Kp*e_P + Ki*e_I + Kd*e_D;
+
+            % 4. Save errors for next time step
+            E_k(1,i)   = e_I;
+            e_k_1(1,i) = e_k;
+
+            if (abs(e_k)>angleThresh) && (dist>distThresh)
+                turnFlag(1,i) = 1;
+                StraightFlag(1,i) = 0;
+                vRight(1,i) = (2*v+w*L)/(2*R);
+                vLeft(1,i)  = (2*v-w*L)/(2*R);    
+            elseif abs(e_k)<angleThresh
+                turnFlag(1,i) = 0;
+                StraightFlag(1,i) = 1;
+            end
+
+            if StraightFlag(1,i)
+                if dist>distThresh
+                    vRight(1,i) = v;
+                    vLeft(1,i) = v;
+                else
+                    if LOOP>=length(trajectory)
+                        vRight(1,i) = 0;
+                        vLeft(1,i) = 0;
+                        moving(1,i) = 0;
+                    end
+                end
+            end
+            addpoints(robotTrajectory(i),x,y);
+            [res Ints Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'setk3Velocity_function',[i vLeft(1,i) vRight(1,i)],[],'',[],vrep.simx_opmode_blocking);
+        end
+        drawnow
+    end
+
+    for i=1:numberOfRobots
+        vLeft(1,i) = 0;
+        vRight(1,i) = 0;
+        [res Ints Floats Strings Buffer]=vrep.simxCallScriptFunction(clientID,'remoteApiCommandServer',vrep.sim_scripttype_childscript,'setk3Velocity_function',[i vLeft(1,i) vRight(1,i)],[],'',[],vrep.simx_opmode_blocking);
+    end
+end
+
 vrep.delete();
